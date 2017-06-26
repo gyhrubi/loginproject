6ccce3663e489572460253e1864babd7c2b2fbf3
Gyuszi <gyhrubi@comforth.hu>
2017-06-26 16:03:00 +0200

    Login page html-css ready

--- resources/js/testing.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.login-btn').addEventListener('click', changePage);
-document.querySelector('.logout-btn').addEventListener('click', changePage);
-
-function changePage() {
-    document.querySelector('.login-page').classList.toggle('show');
-    document.querySelector('.user-page').classList.toggle('show');
-}
