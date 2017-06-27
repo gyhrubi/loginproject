e086c40e5d29f4a7ca430055079340763d6ae96a
Eszti <etetenyi@comforth.hu>
2017-06-27 12:56:14 +0200

    PLussz clearfields(), loginFails() és showUserList

--- resources/js/ui.js
+++ resources/js/ui.js
@@ -5,7 +5,10 @@ var UIController = (function(){
     var DOMstrings = {
         input_username: '#login-field-username',
         input_password: '#login-field-password',
-        login_btn: '.login-btn'
+        login_btn: '.login-btn',
+        loginpage: '.login-page',
+        userpage: '.user-page',
+        errortext: '.login-console-message'
     };
     
     // Public part of the module:
@@ -16,13 +19,40 @@ var UIController = (function(){
             password: document.querySelector(DOMstrings.input_password).value
           };
       }, 
-        // addListItem: function
-        // deleteList item: function
-        // clearFields: function
-        /* displayLogin: function(obj) {
-            document.querySelector(DOMstrings.input_username).textContent = user_name;
-            document.querySelector(DOMstrings.input_password).textContent = password;
-        } */
+        
+        showUserList: function() {
+            var x = document.querySelector(DOMstrings.loginpage).classList;
+            x.remove('show');
+            var y = document.querySelector(DOMstrings.userpage).classList;
+            y.toggle('show');
+        },
+        loginFails: function(error_reason) {
+            var z;
+            z = document.querySelector(DOMstrings.errortext).textContent;
+           switch(error_reason) {
+            case 0:
+                 z = 'Hibás jelszó';
+                break;
+            case 1:
+                z = 'Hibás felhasználó';
+                break;
+            default:
+                 z = 'Hibás bejelentkezés';
+} 
+        },
+        
+        clearFields: function() {
+            var fields, fieldsArr;
+            fields = document.querySelectorAll(DOMstrings.input_username +', '+ DOMstrings.input_password);
+            
+            fieldsArr = Array.prototype.slice.call(fields);
+            fieldsArr.forEach(function(current, index, array) {
+               current.value = ""; 
+            });
+            
+        },
+       
+    
         getDOMstrings: function() {
             return DOMstrings;
         }
