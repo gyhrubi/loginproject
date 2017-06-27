0cc34f852d3165f1b185a28f37056e5c188ab1fc
Gyuszi <gyhrubi@comforth.hu>
2017-06-27 11:23:37 +0200

    EventListeners ready to rock!

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,51 @@
 var AppController = (function(ctrlData,ctrlUI){
 
     // Private part of the module:
-    console.log('Hello World!');
+    var setupEventListeners = function() {
+        
+        var DOM = ctrlUI.getDOMStrings();
+        
+        document.querySelector(DOM.loginBtn).addEventListener('click',ctrlLogin);
+        
+        document.addEventListener('keypress', function(event){
+           
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlLogin();
+            }
+            
+        });
+        
+    };
+    
+    var ctrlLogin = function() {
+      
+        console.log('Login method.');
+        
+        /*
+        // Get login input datas
+        var loginData = ctrlUI.getLoginInput();
+        
+        // Authorize current login
+        var authResult = ctrlData.authUser();
+        
+        // Render the authorization result
+        if (authResult === 1) {
+            ctrlUI.showUserList(param);
+        } else {
+            ctrlUI.loginFails(param);
+        }
+        */
+    };
+    
     
     // Public part of the module:
     return {
-        
+        init: function() {
+            setupEventListeners();
+        }
         
     };
     
 })(DataController,UIController);
+
+AppController.init();
